8d00baac17fa2271e183d7a3a5a204639bf6eacc
R1chardy <yuanrichard33@gmail.com>
2022-10-29 19:25:04 -0500

    UnionFind

---
+++ UnionFind/UnionFind.h
@@ -0,0 +1,31 @@
+//
+// Created by yuanr on 10/29/2022.
+//
+
+#ifndef UNIONFIND_UNIONFIND_H
+#define UNIONFIND_UNIONFIND_H
+
+
+class UnionFind {
+private:
+    int num;
+    int numComp;
+    int* sz;
+    int* root;
+
+public:
+    UnionFind(int n);
+    UnionFind& operator=(const UnionFind& other);
+    UnionFind(const UnionFind& other);
+    ~UnionFind();
+    int find(int x);
+    bool connected(int p, int q);
+    int componentSize(int p);
+    int size();
+    int components();
+    bool unify(int p, int q);
+    void print();
+};
+
+
+#endif //UNIONFIND_UNIONFIND_H
---
+++ UnionFind/cmake-build-debug/CMakeFiles/UnionFind.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnionFind.dir/UnionFind.cpp.o"
+  "CMakeFiles/UnionFind.dir/UnionFind.cpp.o.d"
+  "CMakeFiles/UnionFind.dir/main.cpp.o"
+  "CMakeFiles/UnionFind.dir/main.cpp.o.d"
+  "UnionFind.exe"
+  "UnionFind.pdb"
+  "libUnionFind.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnionFind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
